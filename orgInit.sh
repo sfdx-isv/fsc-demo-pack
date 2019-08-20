5a14eeb64a92644473e597308f10823838eed37a
jrattanpal <jrattanpal@gmail.com>
2019-08-20 17:01:00 -0400

    Updating FSC package ID to latest - Summer'19

--- orgInit.sh
+++ orgInit.sh
@@ -4,7 +4,7 @@
 sfdx force:org:create -f config/project-scratch-def.json -a FSCADK2 --setdefaultusername -d 1
 
 #pckg installs
-sfdx force:package:install --package 04t1E000000tr93 -w 20 
+sfdx force:package:install --package 04t1E000000y9ew -w 20 
 sfdx force:package:install --package 04t1E000001Iql5 -w 20
 
 sfdx force:mdapi:deploy --deploydir mdapi-source/app-config
